8a1924d9fcaf91bc990017b9f656a6382c477cf1
SHRISHTI NEMA <shrishtinema54@gmail.comgit config --global user.email>
2025-04-09 10:51:07 +0530

    typecast

---
+++ typecast.java
@@ -0,0 +1,16 @@
+public class typecast {
+    public static void main (String[] args){
+// automatic conversion
+   byte a = 5;
+   int b = a ;
+   System.out.println(a);
+
+
+   // non automatic conversion 
+   int c = 129;
+   byte d = (byte)c;
+   System.out.println(d); 
+
+    }
+
+}
